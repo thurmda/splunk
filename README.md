e15a76cea1b12cb668b1ddd33295eb41b4a1acaa
Dan Thurman <dan.thurman@buddymedia.com>
2012-12-12 16:28:08 -0500

    examples

---
+++ README.md
@@ -0,0 +1,3 @@
+A working in progress to create a USEFUL splunk search streaming API
+
+    bin/splunk -s 'search tab*.prod POST "/index.php?id=*"' -h 
--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 var splunk = require('./lib/splunk');
-
+var redis = require('redis');
 var rx = /POST.*index.php\?id=(\d+)/;
 
+var redisClient = redis.createClient();
+var count = 0;
+var hits = {};
+redisClient.del('hits:*');
 /*
-//splunk('savedsearch "thurmda1"', function(data){
-//splunk('search tab*.prod POST "/index.php?id=*"', function(data){
-splunk.search('search "POST /fanpage/index.php?id=*"', function(data){
-    console.log(data.length);
-    //data.forEach(function(req){
-    //    var m = rx.exec(req._raw);
-    //    if(m){
-    //        console.dir(m);
-    //    }
-    //});
-});
+redis-cli -h 192.168.10.111 KEYS "hits*" |\
+    xargs redis-cli -h 192.168.10.111 DEL
 */
 
-splunk.stream('savedsearch "thurmda1"', function(){
-
-
-});
+//using stream to argregate data with REDIS
+var stream = splunk.Stream('search tab*.prod POST "/index.php?id=*"', {hostname: '107.21.18.158'});
+    stream.on('object', function(d){
+        count++;
+        var m = rx.exec(d._raw);
+        var key;
+        if(m){
+            key = ['hits' , d._time.substr(0,10), d._time.substr(11,2)].join(':');
+            redisClient.zincrby(key, 1, m[1]);
+        }else{
+            console.log('BOGUS ' + d._raw);
+        }
+        if(count % 100 === 0){
+            console.dir(d);
+        }
+    });
+    stream.on('error', function(d,j){
+        console.log('error');
+        console.dir(arguments);
+    });
